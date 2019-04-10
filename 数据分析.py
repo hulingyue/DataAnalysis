00f5056f2edcaed434e5003b0f9ed33b002ac696
511809125@qq.com <511809125@qq.com>
2019-04-10 15:02:57 +0800

    Initial commit

---
+++ 数据分析.py
@@ -0,0 +1,226 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.datasets import load_breast_cancer
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import mean_squared_error
+
+if True:
+    bool_dealData = False
+    bool_Hour = False
+    bool_2016_01_12 = False
+    bool_2016_01_12_to_19 = False
+
+if bool_dealData:
+    # ipython notebook路径不能出现中文
+    # 1.导入数据
+    data = pd.read_csv(r"G:\energydata_complete.csv")
+    # print(data.describe())
+    # 2.将str格式的时间数据转换成时间格式
+    data['date'] = pd.to_datetime(data['date'], format="%Y-%m-%d %H:%M:%S")
+
+    # 3.增加有效信息
+    df = data.set_index('date')
+
+
+    def Second(x):
+        temp = x.hour * 3600
+        temp += x.minute * 60
+        temp += x.second
+        return temp
+
+
+    data['Msn'] = pd.DataFrame(df.index.map(lambda x: Second(x)))
+    data['week_year'] = pd.DataFrame(df.index.weekofyear)  # 一年当中的第几周
+    data['my'] = pd.DataFrame(df.index.map(lambda x: x.strftime('%Y-%m-%d')))  # 增加my列表示日期，形式例如2019-04-01
+    data['mhr'] = pd.DataFrame(df.index.map(lambda x: x.strftime('%H:00:00')))  # 增加hour列表示日期，形式例如17:00:00
+    data['day_week'] = pd.DataFrame(df.index.day_name())  # 增加星期
+
+
+    def weekend_weekday(x):
+        x = str(x)
+        if "Saturday" in x or "Sunday" in x:
+            return "Weekend"
+        else:
+            return "Weekday"
+
+
+    data['weekStatus'] = pd.DataFrame(list(data['day_week'].map(lambda x: weekend_weekday(x))))
+    data = data.drop(['rv1', 'rv2'], axis=1)  # 移除rv1、rv2
+    data.to_csv("./dataAfterDeal.csv", index=False)
+    # print(data)
+    del data, df
+
+if bool_Hour:
+    data = pd.read_csv("./dataAfterDeal.csv")
+    df = data.set_index('date')
+    df.index = pd.to_datetime(df.index)  # 数据类型转换
+    data = df.resample("H").sum().to_period("H")
+    print(data)
+    # data = data[['Appliances', 'lights']]
+    # 保存Appliances、lights
+    data.to_csv("./powerUserByHour.csv")
+    del data, df
+
+if False:
+    data = pd.read_csv("./powerUserByHour.csv")
+    data['date'] = pd.to_datetime(data['date'], format="%Y-%m-%d %H:%M:%S")
+    df = data.set_index('date')
+
+    data['day_week'] = pd.DataFrame(df.index.day_name())  # 增加星期
+    data['Monday'] = pd.get_dummies(data['day_week'])['Monday']
+    data['Tuesday'] = pd.get_dummies(data['day_week'])['Tuesday']
+    data['Wednesday'] = pd.get_dummies(data['day_week'])['Wednesday']
+    data['Thursday'] = pd.get_dummies(data['day_week'])['Thursday']
+    data['Friday'] = pd.get_dummies(data['day_week'])['Friday']
+    data['Saturday'] = pd.get_dummies(data['day_week'])['Saturday']
+    data['Sunday'] = pd.get_dummies(data['day_week'])['Sunday']
+
+    data['Msn'] = pd.DataFrame(df.index.map(lambda x: x.hour * 3600))
+    data['date'] = pd.DataFrame(df.index.hour)  # 去除日期
+    data = data.drop(['day_week'], axis=1)  # 去除字符串
+    data['week_year'] = pd.DataFrame(df.index.weekofyear)  # 一年当中的第几周
+    # print(data.corr()[u'Appliances'])
+    data = data.drop(['date'], axis=1)
+    data.to_csv("./dataForTrain.csv")
+    del data, df
+
+if True:
+    # import modin.pandas as pd
+
+    data = pd.read_csv("./dataForTrain.csv")
+    data = data.drop(['lights'], axis=1)
+    print(data.columns.values.tolist())
+    x_train, x_test, y_train, y_test = train_test_split(data.drop(['Appliances'], axis=1), data['Appliances'],
+                                                        random_state=1234)
+    print("划分数据集成功......")
+    import os
+    from sklearn.externals import joblib
+    if os.path.exists("gbrt_model.m"):
+        gbrt = joblib.load("gbrt_model.m")
+        print("模型加载成功！")
+        pass
+    else:
+        print("开始训练模型......")
+        gbrt = GradientBoostingClassifier(random_state=1234, learning_rate=1)
+        gbrt.fit(x_train, y_train)
+        joblib.dump(gbrt, "gbrt_model.m")
+
+    train_predict = gbrt.predict(x_train)
+
+    print("%2f" % (gbrt.score(x_train, y_train) * 100), "%")
+    print("%2f" % (gbrt.score(x_test, y_test) * 100), "%")
+    predict = gbrt.predict(x_test)
+    if gbrt.score(x_test, y_test) < 0.1:
+        os.remove('gbrt_model.m')
+
+    # # 平均值绝对误差
+    # print(mean_squared_error(y_test, predict))
+    mes = float()
+    if len(y_test) == len(predict):
+        for i in range(0, len(y_test)):
+            mes = mes + abs(list(y_test)[i] - predict[i])
+    else:
+        raise KeyError((len(y_test), len(predict)))
+    mes = mes / len(y_test)
+    print("Mes", mes)
+
+    # for i in range(10, 100):
+    #     gbrt = GradientBoostingClassifier(n_estimators=i, random_state=1234, learning_rate=0.1)
+    #     gbrt.fit(x_train, y_train)
+    #     train_predict = gbrt.predict(x_train)
+    #
+    #     print(i)
+    #     print("%2f" % (gbrt.score(x_train, y_train) * 100), "%")
+    #     print("%2f" % (gbrt.score(x_test, y_test) * 100), "%")
+    #     predict = gbrt.predict(x_test)
+    #
+    #     # # 平均值绝对误差
+    #     # print(mean_squared_error(y_test, predict))
+    #     mes = float()
+    #     if len(y_test) == len(predict):
+    #         for i in range(0, len(y_test)):
+    #             mes = mes + abs(list(y_test)[i] - predict[i])
+    #     else:
+    #         raise KeyError((len(y_test), len(predict)))
+    #     mes = mes / len(y_test)
+    #     print("Mes", mes)
+
+    # for i in range(110, 200):
+    #     gbrt = GradientBoostingClassifier(n_estimators=i, learning_rate=0.05, min_samples_split=30, max_features='sqrt',
+    #                                       subsample=0.8, max_depth=4, random_state=1234)
+    #     gbrt.fit(x_train, y_train)
+    #     gbrt.predict(x_test)
+    #     test_score = gbrt.score(x_test, y_test)
+    #     if test_score > 0.50:
+    #         print(i, ":")
+    #         print(gbrt.score(x_train, y_train))
+    #         print(gbrt.score(x_test, y_test))
+    #     predict = gbrt.predict(x_test)
+    #     print(mean_squared_error(y_test, predict))
+
+    '''
+        使用示例
+    '''
+    # cancer = load_breast_cancer()
+    # x_train, x_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
+    # gbrt = GradientBoostingClassifier()
+    # gbrt.fit(x_train, y_train)
+    # print(gbrt.score(x_train, y_train))
+    # print(gbrt.score(x_test, y_test))
+    #
+    # gbrt = GradientBoostingClassifier(n_estimators=100, learning_rate=0.01)
+
+    # print(data)
+    del data
+
+if bool_2016_01_12:
+    # 绘制 2016-01-12 lights 随时间变化曲线
+    '''
+    结论：
+        0点、5-8点、16-18点、20-23点为lights的用电高峰
+    '''
+    appliances_light = df.resample('H').sum().to_period('H')[7:31][["Appliances", "lights"]]
+    plt.plot(appliances_light.index.hour, appliances_light['lights'], c='r')
+    # plt.scatter(appliances_light.index.hour, appliances_light['lights'], c='r')
+    plt.xticks(appliances_light.index.hour)
+    plt.title("2016-01-12 lights")
+    plt.savefig("2016-01-12 lights.png")
+    # plt.show()
+    plt.cla()
+
+if bool_2016_01_12_to_19:
+    # 绘制2016-01-12 - 2016-01-19 lights随时间变化曲线
+    '''
+    结论：
+        显然也是3个小高峰
+    '''
+    appliances_light = df.resample('H').sum().to_period('H')[7:31][["Appliances", "lights"]]
+    plt.scatter(appliances_light.index.hour, appliances_light['lights'], c='b')
+
+    appliances_light = df.resample('H').sum().to_period('H')[31:55][["Appliances", "lights"]]
+    plt.scatter(appliances_light.index.hour, appliances_light['lights'], c='g')
+
+    appliances_light = df.resample('H').sum().to_period('H')[55:79][["Appliances", "lights"]]
+    plt.scatter(appliances_light.index.hour, appliances_light['lights'], c='r')
+
+    appliances_light = df.resample('H').sum().to_period('H')[79:103][["Appliances", "lights"]]
+    plt.scatter(appliances_light.index.hour, appliances_light['lights'], c='c')
+
+    appliances_light = df.resample('H').sum().to_period('H')[103:127][["Appliances", "lights"]]
+    plt.scatter(appliances_light.index.hour, appliances_light['lights'], c='m')
+
+    appliances_light = df.resample('H').sum().to_period('H')[127:151][["Appliances", "lights"]]
+    plt.scatter(appliances_light.index.hour, appliances_light['lights'], c='y')
+
+    appliances_light = df.resample('H').sum().to_period('H')[151:175][["Appliances", "lights"]]
+    plt.scatter(appliances_light.index.hour, appliances_light['lights'], c='k')
+
+    plt.xticks(appliances_light.index.hour)
+    plt.title("2016-01-12 lights")
+    plt.savefig("2016-01-12 to 2016-01-19 lights.png")
+    # plt.show()
+    plt.cla()
